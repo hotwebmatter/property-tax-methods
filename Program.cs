f91a8cd9daee1bee8e44c82b1a92a1d91e8ca989
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-24 10:16:00 -0500

    Initial commit of property-tax-methods project scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace property_tax_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
