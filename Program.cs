7ae423067cbf61ebf858805c32b21b01a4a634a2
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-24 10:35:05 -0500

    Add another constant, per spec

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace property_tax_methods
     {
         // declare class-level constants
         const double EXEMPTION = 25000,
-                     MILLAGE_RATE = .01003;
+                     MILLAGE_RATE = .01003,
+                     INCREASE_RATE = 2.7;
         static void Main(string[] args)
         {
             WriteLine("{0}{1:C}", "Millage per $1000:", 1000 * MILLAGE_RATE);
